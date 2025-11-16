3d4c3e0c8538dd9b360de970d874a9a25a4187c4
anagondibalu0367-cloud <anagondibalu0367@gmail.com>
2025-11-16 20:47:39 +0530

    Add files via upload

---
+++ WhileloopEX.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    printf("Numbers from 1 to 10:\n");
+
+    // Using while loop
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;  // increment i by 1
+    }
+
+    return 0;
+}
+
