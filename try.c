6ebbfdc879f2cee17dc7ca75baa91164d6e4c327
anagondibalu0367-cloud <anagondibalu0367@gmail.com>
2025-11-16 21:02:33 +0530

    Add files via upload

---
+++ try.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("Hii");
+	unsigned int a = -10;
+	printf("%d",a);
+	double d = 123.00000;
+	printf("%lf\n",d);
+	
+	printf("%&\n",sizeof(long));
+	return 0;
+}
