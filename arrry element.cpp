d01fa79a864797a8b2d7a81624a51c865467dc1a
anagondibalu0367-cloud <anagondibalu0367@gmail.com>
2025-11-16 20:57:30 +0530

    Add files via upload

---
+++ arrry element.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num[4];
+
+			
+	num[0]=24;
+	num[1]=45;
+	num[2]=50;
+	num[3]=36;
+	
+	printf("Array Elements are:\n");
+	for(int i=0; i<4; i++)
+	{
+		printf("%d ",num[i]);
+	}
+  return 0;	
+}
