1639fdc771d2fa943fbc071641ca92997bef2313
anagondibalu0367-cloud <anagondibalu0367@gmail.com>
2025-11-16 12:42:13 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    char a,b,c;
+    printf("Enter the character of a:\n ");
+    scanf("%c",&a);
+    printf("Enter the character of b:  ");
+    scanf("%c",&b);
+
+    printf("Number of a is %c",a);
+    printf("Number of a is %c",b);
+  
+    
+}
